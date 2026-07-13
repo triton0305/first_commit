3e65d33f024a772752689a24c15cc2236d1e53d8
triton0305 <triton0305@gmail.com>
2026-07-13 16:02:26 +0900

    Add pyramid pattern

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,19 @@
 
 int main(void){
    
-    
+    int n;
+    printf("층수를 입력하세요 : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++){
+        for(int j = n; j>i; j--){
+            printf(" ");
+        }
+        for(int k = 1; k <= i*2 - 1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
     
     
     return 0;
