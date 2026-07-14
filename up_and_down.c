9412ca387d32b4a1d52013d18122d5b4a92abb9e
triton0305 <triton0305@gmail.com>
2026-07-14 19:39:17 +0900

    study_random

--- up_and_down.c
+++ up_and_down.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    int age;
+    /*int age;
 
     printf("May i ask how old you are?  : ");
-    scanf("&d", &age);
-
-    // if (조건) {  ...  }  else {   ...   }
-    // if (age >= 20){
-    //     printf("일반인 입니다.");
-    // }
-
-    // else{
-    //     printf("학생입니다");
-    // }
+    scanf("&d", &age);*/
 
     // elementary(8-13) / middle(14-16) / high (17-19)
     // if / else if/ else
     
-    if (age >= 8 && age <=13){
+    /*if (age >= 8 && age <=13){
         printf("you are a elementary school student");
     }
     else if (age >= 14 && age <= 16){
@@ -29,7 +20,21 @@ int main(void){
     }
     else {
         printf("you are no longer a student");
+    }*/
+
+
+    // break / continue
+    // 1번부터 30번까지 있는 반에서 1번에서 5번까지 조별발표
+
+    for (int i=1;i<=30;i++){
+        if (i>=6){
+            printf("나머지 학생은 come back 하세요.^,^\n");
+            break;
+        }
+        printf("%d번 학생은 조별 발표를 준비하세요.\n", i);
     }
+
+
 
 
     return 0;
